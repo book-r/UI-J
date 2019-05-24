99f05f6a4185e55edbc9285d2f40f09f9489ec18
JakeGrath <J.A.McGrath@outlook.com>
2019-05-24 12:16:29 -0500

    Updated About-Us Book with links to GitHub

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -3,10 +3,8 @@ class Carousel {
         this.carousel = object;
         this.images = this.carousel.getElementsByTagName('img')
         this.curIndex = this.images.length - 1
-        console.log(this.curIndex)
         this.left = this.carousel.querySelector('.left-button')
         this.right = this.carousel.querySelector('.right-button')
-        console.log(this.left)
         this.images[this.curIndex].classList.add('show')
         this.left.addEventListener('click', () => this.clickLeft(this.images))
         this.right.addEventListener('click', () => this.clickRight(this.images))
@@ -24,7 +22,6 @@ class Carousel {
         this.images[this.curIndex].classList.add('show')
         this.images[this.curIndex].classList.remove('hide')
         fadeIn(this.images[this.curIndex])
-        console.log(this.curIndex)
         this.updateCarouselText(this.curIndex)
     }
     clickRight(object) {
@@ -40,11 +37,9 @@ class Carousel {
         this.images[this.curIndex].classList.add('show')
         this.images[this.curIndex].classList.remove('hide')
         fadeIn(this.images[this.curIndex])
-        console.log(this.curIndex)
         this.updateCarouselText(this.curIndex)
     }
     updateCarouselText(index) {
-        console.log(index)
         document.querySelectorAll('.carousel-text').forEach(object => {
             object.classList.remove('show-carousel-text')
             if (object.dataset.id === String(index + 1)) {
